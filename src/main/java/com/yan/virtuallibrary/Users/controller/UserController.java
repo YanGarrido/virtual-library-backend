c61813d5034b1561717ad3fb78933c6827b11d53
yangarrido <yangarridodev@gmail.com>
2026-01-29 19:29:33 -0300

    Refactor User module structure

---
+++ src/main/java/com/yan/virtuallibrary/Users/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.yan.virtuallibrary.Users.controller;
+
+import com.yan.virtuallibrary.Users.domain.UserEntity;
+import com.yan.virtuallibrary.Users.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/")
+    public ResponseEntity<Object> createUser(@RequestBody UserEntity userEntity) {
+        try{
+            var result = this.userService.execute(userEntity);
+            return ResponseEntity.ok().body(result);
+        } catch(Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
+    }
+}
