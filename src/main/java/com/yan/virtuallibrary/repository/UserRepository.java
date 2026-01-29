6bae8b3b28d4505b39d863be20217b5db7414256
yangarrido <yangarridodev@gmail.com>
2026-01-29 19:29:48 -0300

    Update existing files with modifications

--- src/main/java/com/yan/virtuallibrary/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yan.virtuallibrary.repository;
-
-import com.yan.virtuallibrary.domain.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-}
