465e111189fba0d839809036f1464bba315ff73f
yangarrido <yangarridodev@gmail.com>
2026-01-29 21:13:13 -0300

    Fix UserBookEntity associations and clean up imports; minor updates to ReadFormat and ReadStatus

--- src/main/java/com/yan/virtuallibrary/domain/ReadFormat.java
+++ src/main/java/com/yan/virtuallibrary/domain/ReadFormat.java
@@ -4,5 +4,6 @@ public enum ReadFormat {
 
     KINDLE,
     PHYSIC,
-    PDF
+    PDF,
+    AUDIOBOOK
 }
--- src/main/java/com/yan/virtuallibrary/domain/ReadStatus.java
+++ src/main/java/com/yan/virtuallibrary/domain/ReadStatus.java
@@ -2,7 +2,7 @@ package com.yan.virtuallibrary.domain;
 
 public enum ReadStatus {
 
-    READ,
     READING,
-    WANT_TO_READ
+    FINISHED,
+    WISHLIST
 }
