28d0b162a658e2124b5e3a30ad5a7f52dd5a0387
yangarrido <yangarridodev@gmail.com>
2026-02-05 23:08:26 -0300

    refactor(users): remove unused imports

--- src/main/java/com/yan/virtuallibrary/Users/dto/UserResponseDTO.java
+++ src/main/java/com/yan/virtuallibrary/Users/dto/UserResponseDTO.java
@@ -1,8 +1,5 @@
 package com.yan.virtuallibrary.Users.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-
 public record UserResponseDTO(
         Long id,
         String name,
