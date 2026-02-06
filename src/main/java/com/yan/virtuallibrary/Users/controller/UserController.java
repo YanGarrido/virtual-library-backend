dbf2e94a415434d093693ccf7f1f407ac8804836
yangarrido <yangarridodev@gmail.com>
2026-02-05 23:07:17 -0300

    feat(users): implement the valid annotation for better validation

--- src/main/java/com/yan/virtuallibrary/Users/controller/UserController.java
+++ src/main/java/com/yan/virtuallibrary/Users/controller/UserController.java
@@ -3,6 +3,7 @@ package com.yan.virtuallibrary.Users.controller;
 import com.yan.virtuallibrary.Users.dto.UserRequestDTO;
 import com.yan.virtuallibrary.Users.dto.UserResponseDTO;
 import com.yan.virtuallibrary.Users.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +16,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    public ResponseEntity<?> create(@RequestBody UserRequestDTO userRequestDTO) {
+    public ResponseEntity<?> create(@Valid @RequestBody UserRequestDTO userRequestDTO) {
         try{
             UserResponseDTO response = this.userService.execute(userRequestDTO);
             return ResponseEntity.status(201).body(response);
@@ -36,7 +37,7 @@ public class UserController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> updateUser(@PathVariable Long id, @RequestBody UserRequestDTO userRequestDTO){
+    public ResponseEntity<?> updateUser(@Valid  @PathVariable Long id, @RequestBody UserRequestDTO userRequestDTO){
         try{
             UserResponseDTO response = this.userService.updateUser(id, userRequestDTO);
             return ResponseEntity.ok().body(response);
