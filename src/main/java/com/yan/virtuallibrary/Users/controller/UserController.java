004240f06e302333b1a49f5ba4bf23eb12c65500
yangarrido <yangarridodev@gmail.com>
2026-02-05 22:57:40 -0300

    feat(users): add REST endpoints for create, get and update users

--- src/main/java/com/yan/virtuallibrary/Users/controller/UserController.java
+++ src/main/java/com/yan/virtuallibrary/Users/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.yan.virtuallibrary.Users.controller;
 
-import com.yan.virtuallibrary.Users.domain.entities.UserEntity;
+import com.yan.virtuallibrary.Users.dto.UserRequestDTO;
+import com.yan.virtuallibrary.Users.dto.UserResponseDTO;
 import com.yan.virtuallibrary.Users.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,14 +14,36 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/")
-    public ResponseEntity<Object> createUser(@RequestBody UserEntity userEntity) {
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody UserRequestDTO userRequestDTO) {
         try{
-            var result = this.userService.execute(userEntity);
-            return ResponseEntity.ok().body(result);
+            UserResponseDTO response = this.userService.execute(userRequestDTO);
+            return ResponseEntity.status(201).body(response);
         } catch(Exception e){
             return ResponseEntity.badRequest().body(e.getMessage());
         }
 
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable Long id){
+        try{
+            UserResponseDTO response = this.userService.getUserById(id);
+            return ResponseEntity.ok().body(response);
+        } catch (Exception e){
+        return  ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable Long id, @RequestBody UserRequestDTO userRequestDTO){
+        try{
+            UserResponseDTO response = this.userService.updateUser(id, userRequestDTO);
+            return ResponseEntity.ok().body(response);
+
+        } catch (Exception e){
+            return  ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
 }
